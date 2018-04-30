6274dde8c6a0d0c551260be7c2154ac83746fa4e
liuren <liuren@cc1st.com>
2018-04-30 18:41:32 +0800

    查询评论页面

--- app.js
+++ app.js
@@ -1,12 +1,44 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const views = require('koa-views');
+const mongoose = require('mongoose');
 const app = new Koa();
+let router = new Router();
 
-const host=`3000`;
+const port = process.env.PORT || 3000;
+const host = `127.0.0.1`;
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+mongoose.connect(`mongodb://${host}/passport`);
+let db = mongoose.connection;
+
+db.once('open', () => {
+  console.log('Connected to Mongodb');
 });
 
-app.listen(host);
+db.on('error', (err) => {
+  console.log(err);
+});
 
-console.log(`passport-portal is run http://127.0.0.1:${host}`);
+app.use(views(__dirname + '/views', {extension: 'pug'}));
+
+let Article = require('./models/article');
+
+Article.find({}, async (err, articles) => {
+  await router.get('/', async (ctx, next) => {
+    await ctx.render('index', {
+      title: 'Hello Koa 2!',
+      articles
+    });
+  })
+});
+
+app.use(router.routes());
+
+app.listen(port, () => {
+  console.log(`passport-portal listening on port http://${host}:${port}`);
+});
+
+
+
+
+
